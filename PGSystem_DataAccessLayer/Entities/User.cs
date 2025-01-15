cd69955dce821f7a1bc4f979155ab4995774d0ea
Sam <61649338+thanhdong06@users.noreply.github.com>
2025-01-15 15:36:29 +0700

    Init Database

---
+++ PGSystem_DataAccessLayer/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGSystem_DataAccessLayer.Entities
+{
+    internal class User
+    {
+    }
+}
