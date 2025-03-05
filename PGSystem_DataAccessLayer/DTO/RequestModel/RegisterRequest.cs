b8c8906314398592549f9da330f5d3fbf2c1d1bd
Sam <61649338+thanhdong06@users.noreply.github.com>
2025-03-05 15:24:50 +0700

    Update Register

--- PGSystem_DataAccessLayer/DTO/RequestModel/RegisterRequest.cs
+++ PGSystem_DataAccessLayer/DTO/RequestModel/RegisterRequest.cs
@@ -11,5 +11,6 @@ namespace PGSystem_DataAccessLayer.DTO.RequestModel
         public string Email { get; set; }
         public string Password { get; set; }
         public string Phone {  get; set; }
+        public string FullName { get; set; }
     }
 }
